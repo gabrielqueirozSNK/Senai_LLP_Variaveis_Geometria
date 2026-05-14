42fe3375c7d41d1a9fb445f7e1fb6924da3ee02c
gabrielqueirozSNK <gabrielqueirozfaculdade151a@gmail.com>
2026-05-14 20:20:00 -0300

    Bola.java

--- Bola.java
+++ Bola.java
@@ -1,6 +1,14 @@
-
 package senai_llp_variaveis_geometria;
-//SNK PRODUCTIONS
+import java.util.Scanner;
+import java.util.Locale;
 public class Bola {
-    
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in).useLocale(Locale.US);
+        System.out.print("digite o raio da bola: ");
+        double raio = leitor.nextDouble();
+        double area = 4 * Math.PI * Math.pow(raio, 2);
+        System.out.printf("a area da superficie da bola é: %.2f%n", area);
+        leitor.close();
+    }
 }
+//SNK PRODUCTIONS
